6092b6411bc6cd5e07d22e2f1073a0944a633530
Darnell Andries <dandries@brave.com>
2024-11-01 16:30:49 -0700

    Consolidate migrations, update README

---
+++ migrations/20241021231751_init.down.sql
@@ -0,0 +1,6 @@
+DROP TABLE verifications;
+DROP TABLE sessions;
+DROP TABLE registration_states;
+DROP TABLE ake_states;
+DROP TABLE accounts;
+DROP TABLE oprf_seeds;
--- migrations/20241021231751_init.up.sql
+++ migrations/20241021231751_init.up.sql
@@ -1,8 +1,30 @@
+CREATE TABLE oprf_seeds (
+    id SERIAL PRIMARY KEY,
+    seed BYTEA NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE accounts (
     id UUID PRIMARY KEY,
     email TEXT NOT NULL,
+    oprf_seed_id INT REFERENCES oprf_seeds(id),
+    opaque_registration BYTEA,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     UNIQUE(email)
+);
+
+CREATE TABLE ake_states (
+    id UUID PRIMARY KEY,
+    account_id UUID REFERENCES accounts(id),
+    oprf_seed_id INT REFERENCES oprf_seeds(id),
+    state BYTEA NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE registration_states (
+    email TEXT PRIMARY KEY,
+    oprf_seed_id INT REFERENCES oprf_seeds(id),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE sessions (
