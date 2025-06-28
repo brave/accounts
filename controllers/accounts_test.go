c12e2be8864bccf26d1af2dbf5084a534d8e079f
Darnell Andries <dandries@brave.com>
2025-06-27 17:14:04 -0700

    Update test to account for password change alert email

--- controllers/accounts_test.go
+++ controllers/accounts_test.go
@@ -328,6 +328,9 @@ func (suite *AccountsTestSuite) TestChangePassword() {
 		account, err := suite.ds.GetOrCreateAccount(email)
 		suite.Require().NoError(err)
 
+		err = suite.ds.SetAccountLocaleIfMissing(account.ID, "fr-FR")
+		suite.Require().NoError(err)
+
 		// Create sessions to verify session invalidation or lack thereof
 		for range 3 {
 			_, err := suite.ds.CreateSession(account.ID, datastore.EmailAuthSessionVersion, "")
@@ -368,6 +371,8 @@ func (suite *AccountsTestSuite) TestChangePassword() {
 		})
 		serializedChangeRecord := hex.EncodeToString(changeRecord.Serialize())
 
+		suite.sesMock.On("SendPasswordChangeNotification", mock.Anything, email, "fr-FR").Return(nil).Once()
+
 		// Test password change finalize with session invalidation
 		req = util.CreateJSONTestRequest("/v2/accounts/password/finalize", controllers.RegistrationRecord{
 			SerializedRecord:   &serializedChangeRecord,
@@ -383,6 +388,8 @@ func (suite *AccountsTestSuite) TestChangePassword() {
 		suite.False(changeFinalizeResp.RequiresTwoFA)
 		suite.False(changeFinalizeResp.RequiresEmailVerification)
 		suite.Equal(sessionInvalidation, changeFinalizeResp.SessionsInvalidated)
+
+		suite.sesMock.AssertExpectations(suite.T())
 
 		// Verify the account still exists and password was changed
 		updatedAccount, err := suite.ds.GetAccount(nil, email)
